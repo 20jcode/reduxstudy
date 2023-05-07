3d360d07e32ac824747af7b46a1d534a54e464f0
20jcode <20j.code@gmail.com>
2023-05-07 17:37:02 +0900

    compile error

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-import Counter from "./components/Counter";
+
+import CounterContainer from './containers/CounterContainer';
+import TodosContainer from './containers/TodosContainer';
 
 const App = () => {
   return(
     <div>
-      <Counter number={0} />
+      <CounterContainer />
+      <hr />
+      <TodosContainer />
     </div>
   );
 };
--- src/components/Todos.js
+++ src/components/Todos.js
@@ -1,11 +1,52 @@
 const TodoItem = ({ todo, onToggle, onRemove}) => {
     return (
         <div>
-            <input type="checkbox" />
-            <span>test text</span>
-            <button>del</button>
+            <input 
+                type="checkbox"
+                onClick={() => onToggle(todo.id)}
+                checked={todo.done}
+                readonly={true}
+            />
+            <span style={{ textDecoration: todo.done ? 'line-through' : 'none '}}>
+                {todo.text}
+            </span>
+            <button onClick={() => onRemove(todo.id)}>del</button>
         </div>
     );
 };
 
-const Todos = ({})
+const Todos = ({
+    input,
+    todos,
+    onChangeInput,
+    oninsert,
+    onToggle,
+    onRemove,
+}) => {
+    const onSubmit = e => {
+        e.preventDefault();
+        oninsert(input);
+        onChangeInput('');
+    };
+    const onChange = e => onChangeInput(e.target.value);
+    return (
+        <div>
+            <form onSubmit={onSubmit}>
+                <input value={input} onChange={onChange}/>
+                <button type="submit">등록</button>
+            </form>
+            <div>
+                {todos.map(todo => (
+                    <TodoItem
+                        todo={todo}
+                        key={todo.id}
+                        onToggle={onToggle}
+                        onRemove={onRemove}
+                    />
+                ))}
+            </div>
+        </div>
+    );
+};
+
+export default Todos;
