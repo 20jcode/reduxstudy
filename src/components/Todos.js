84e990ab3b6466ca839f42710256942b5f66550f
20jcode <20j.code@gmail.com>
2023-05-07 15:32:59 +0900

    test1

---
+++ src/components/Todos.js
@@ -0,0 +1,11 @@
+const TodoItem = ({ todo, onToggle, onRemove}) => {
+    return (
+        <div>
+            <input type="checkbox" />
+            <span>test text</span>
+            <button>del</button>
+        </div>
+    );
+};
+
+const Todos = ({})
